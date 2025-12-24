23f602b9f9fddb87074e7a7e45995142e021b3ba
Can Akdag <canakdag06@gmail.com>
2025-12-24 10:07:46 +0300

    BlockHit.cs

---
+++ Assets/Scripts/BlockHit.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class BlockHit : MonoBehaviour
+{
+    [SerializeField] private Sprite emptyBlock;
+    [SerializeField] private int maxHits = -1;
+
+    private SpriteRenderer spriteRenderer;
+
+    private bool isAnimating = false;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isAnimating && maxHits != 0 && collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.transform.DotTest(transform, Vector2.up))
+            {
+                Hit();
+            }
+        }
+    }
+
+    private void Hit()
+    {
+        maxHits--;
+
+        if (maxHits == 0)
+        {
+            spriteRenderer.sprite = emptyBlock;
+        }
+
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        isAnimating = true;
+
+        Vector3 initialPosition = transform.localPosition;
+        Vector3 secondPosition = initialPosition + Vector3.up * 0.5f;
+
+        yield return Move(initialPosition, secondPosition);
+        yield return Move(secondPosition, initialPosition);
+
+        isAnimating = false;
+    }
+
+    private IEnumerator Move(Vector3 from, Vector3 to)
+    {
+        float elapsed = 0;
+        float duration = 0.125f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+
+            transform.localPosition = Vector3.Lerp(from, to, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = to;
+    }
+}
