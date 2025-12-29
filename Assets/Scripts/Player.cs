08a22d755a32ee5d5c461233513c09341376f8d8
Can Akdag <canakdag06@gmail.com>
2025-12-29 14:54:05 +0300

    mario is not vulnerable during scale animation anymore

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -84,6 +84,7 @@ public class Player : MonoBehaviour
     {
         float duration = 1f;
         float endTime = Time.time + duration;
+        capsuleCollider.enabled = false;
 
         while (Time.time < endTime)
         {
@@ -96,6 +97,7 @@ public class Player : MonoBehaviour
         smallRenderer.enabled = false;
         bigRenderer.enabled = false;
         activeRenderer.enabled = true;
+        capsuleCollider.enabled = true;
     }
 
     private IEnumerator StarPowerAnimation()
