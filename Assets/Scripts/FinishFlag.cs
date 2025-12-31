4e4c94ea68d45277f6be69b0ee6ba10d9083e2db
Can Akdag <canakdag06@gmail.com>
2025-12-31 13:18:59 +0300

    created FinishFlag.cs

---
+++ Assets/Scripts/FinishFlag.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class FinishFlag : MonoBehaviour
+{
+    [SerializeField] private Transform flag;
+    [SerializeField] private Transform end;
+    [SerializeField] private Transform castle;
+    [SerializeField] private float speed = 6f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(MoveTo(flag, end.position));
+            StartCoroutine(LevelCompleteSequence(collision.transform));
+        }
+    }
+
+    private IEnumerator LevelCompleteSequence(Transform player)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+
+        yield return MoveTo(player, end.position);
+        yield return MoveTo(player, player.position + Vector3.right);
+        yield return MoveTo(player, player.position + Vector3.right + Vector3.down);
+        yield return MoveTo(player, castle.position);
+
+        player.gameObject.SetActive(false);
+    }
+
+    private IEnumerator MoveTo(Transform subject, Vector3 target)
+    {
+        while ((subject.position - target).sqrMagnitude > 0f)
+        {
+            subject.position = Vector3.MoveTowards(subject.position, target, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        subject.position = target;
+    }
+}
