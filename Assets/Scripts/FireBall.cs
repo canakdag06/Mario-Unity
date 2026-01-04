209ef8101cd82cdc5c87a7c334562d8c6169594d
Can Akdag <canakdag06@gmail.com>
2026-01-04 19:12:59 +0300

    fireball collision check

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -4,6 +4,12 @@ public class FireBall : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     private Vector2 direction;
+    ObjectPool pool;
+
+    private void Awake()
+    {
+        pool = PoolManager.Instance.fireballPool;
+    }
 
     private void Update()
     {
@@ -12,21 +18,19 @@ public class FireBall : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        ReturnToPool();
+        if (collision.gameObject.TryGetComponent(out Goomba goomba)) goomba.GetHit();
+        else if (collision.gameObject.TryGetComponent(out Koopa koopa)) koopa.GetHit();
+
+        pool.ReturnToPool(gameObject);
     }
 
     private void OnBecameInvisible()
     {
-        ReturnToPool();
+        pool.ReturnToPool(gameObject);
     }
 
     public void SetDirection(Vector2 dir)
     {
         direction = dir;
-    }
-
-    private void ReturnToPool()
-    {
-        gameObject.SetActive(false);
     }
 }
